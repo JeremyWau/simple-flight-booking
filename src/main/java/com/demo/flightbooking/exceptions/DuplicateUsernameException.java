a43baa19fe7d4e79820ffbf015fb8e1b2e1a2648
Jeremy W. <jeremy.wautelet@gmail.com>
2026-04-23 21:40:12 +0200

    Small formatting fixes.

--- src/main/java/com/demo/flightbooking/exceptions/DuplicateUsernameException.java
+++ src/main/java/com/demo/flightbooking/exceptions/DuplicateUsernameException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public class DuplicateUsernameException extends HttpRuntimeException {
     public DuplicateUsernameException(String message) {
-        super("Error, the username is already in use: "+message);
+        super("Error, the username is already in use: " + message);
     }
 
     @Override
